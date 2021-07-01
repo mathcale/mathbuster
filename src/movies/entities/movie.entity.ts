40e877436337d7ebec0e1072051cc0a39f4d45ff
Matheus Calegaro <matheuscalegaro@outlook.com>
2021-07-01 01:01:11 -0300

    feat(movies): declaring Movie entity

--- src/movies/entities/movie.entity.ts
+++ src/movies/entities/movie.entity.ts
@@ -1,1 +1,56 @@
-export class Movie {}
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  ObjectID,
+  ObjectIdColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('movies')
+export class Movie {
+  @ObjectIdColumn()
+  id: ObjectID;
+
+  @Column({ nullable: false })
+  @Index({ unique: true })
+  title: string;
+
+  @Column({ nullable: false })
+  overview: string;
+
+  @Column({ nullable: false })
+  genres: string[];
+
+  @Column({ nullable: false })
+  releaseDate: string;
+
+  @Column({ nullable: false })
+  isAdult: boolean;
+
+  @Column({ nullable: false })
+  availableCopies: number;
+
+  @Column({ nullable: true })
+  imageUrl?: string;
+
+  @Column({ nullable: false, select: false })
+  enabled: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  constructor(movie?: Partial<Movie>) {
+    Object.assign(this, movie);
+  }
+
+  @BeforeInsert()
+  beforeInsertActions() {
+    this.enabled = true;
+  }
+}
