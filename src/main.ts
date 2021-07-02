6e685dd663f3e2f93987fe396ab338d132647465
Matheus Calegaro <matheuscalegaro@outlook.com>
2021-07-02 00:52:23 -0300

    chore(app): configuring swagger

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerCustomOptions,
+} from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -13,6 +18,21 @@ async function bootstrap() {
     new FastifyAdapter({ logger: true }),
   );
 
+  const config = new DocumentBuilder()
+    .setTitle('Mathbuster')
+    .setDescription(
+      "An old-school movie rental app, built with modern tech for Digital Ocean's 2021 MongoDB Hackathon",
+    )
+    .setVersion('1.0')
+    .addTag('movies', 'Describes the Movie resource')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  const customOptions: SwaggerCustomOptions = {
+    customSiteTitle: 'Mathbuster API Reference',
+  };
+
+  SwaggerModule.setup('docs', app, document, customOptions);
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
