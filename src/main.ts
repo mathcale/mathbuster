e924e8aa02991d3e304f92f782211e1ba30f9235
Matheus Calegaro <matheuscalegaro@outlook.com>
2021-07-05 02:15:10 -0300

    chore: dockerizing the app

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,7 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document, customOptions);
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 
 bootstrap();
