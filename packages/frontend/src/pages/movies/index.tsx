01f68439b3eabf7e4b458b85a44808e744819580
Matheus Calegaro Lopes da Silva <matheus.calegaro@amedigital.com>
2021-07-11 20:11:48 -0300

    feat(frontend/movies): adding table with pagination on movies list

--- packages/frontend/src/pages/movies/index.tsx
+++ packages/frontend/src/pages/movies/index.tsx
@@ -1,7 +1,100 @@
 import { useState, useEffect, useMemo } from 'react';
 import Head from 'next/head';
-import { Container } from 'reactstrap';
-import { useTable, usePagination } from 'react-table';
+import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import Chip from '@material-ui/core/Chip';
+
+const useStyles1 = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexShrink: 0,
+      marginLeft: theme.spacing(2.5),
+    },
+  })
+);
+
+interface TablePaginationActionsProps {
+  count: number;
+  page: number;
+  rowsPerPage: number;
+  onPageChange: (event: React.MouseEvent<HTMLButtonElement>, newPage: number) => void;
+}
+
+function TablePaginationActions(props: TablePaginationActionsProps) {
+  const classes = useStyles1();
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <div className={classes.root}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+
+      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </div>
+  );
+}
+
+const useStyles2 = makeStyles({
+  table: {
+    minWidth: 500,
+  },
+});
 
 export default function ListMoviesPage() {
   // FIXME: use correct type
@@ -9,175 +102,20 @@ export default function ListMoviesPage() {
   const [page, setPage] = useState<number>(1);
   const [limit, setLimit] = useState<number>(10);
 
+  const [rowsPerPage, setRowsPerPage] = useState(10);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const classes = useStyles2();
 
-  function Table({ columns, data, fetchData, loading, pageCount: controlledPageCount }) {
-    const {
-      getTableProps,
-      getTableBodyProps,
-      headerGroups,
-      prepareRow,
-      page,
-      canPreviousPage,
-      canNextPage,
-      pageOptions,
-      pageCount,
-      gotoPage,
-      nextPage,
-      previousPage,
-      setPageSize,
-      // Get the state from the instance
-      state: { pageIndex, pageSize },
-    } = useTable(
-      {
-        columns,
-        data,
-        initialState: { pageIndex: 0 }, // Pass our hoisted table state
-        manualPagination: true, // Tell the usePagination
-        // hook that we'll handle our own data fetching
-        // This means we'll also have to provide our own
-        // pageCount.
-        pageCount: controlledPageCount,
-      },
-      usePagination
-    );
+  const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+    setPage(newPage);
+  };
 
-    // Listen for changes in pagination and use the state to fetch our new data
-    useEffect(() => {
-      fetchData({ pageIndex, pageSize });
-    }, [fetchData, pageIndex, pageSize]);
-
-    // Render the UI for your table
-    return (
-      <>
-        <pre>
-          <code>
-            {JSON.stringify(
-              {
-                pageIndex,
-                pageSize,
-                pageCount,
-                canNextPage,
-                canPreviousPage,
-              },
-              null,
-              2
-            )}
-          </code>
-        </pre>
-        <table {...getTableProps()}>
-          <thead>
-            {headerGroups.map(headerGroup => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map(column => (
-                  <th {...column.getHeaderProps()}>
-                    {column.render('Header')}
-                    <span>{column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}</span>
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {page.map((row, i) => {
-              prepareRow(row);
-              return (
-                <tr {...row.getRowProps()}>
-                  {row.cells.map(cell => {
-                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
-                  })}
-                </tr>
-              );
-            })}
-            <tr>
-              {loading ? (
-                // Use our custom loading state to show a loading indicator
-                <td colSpan="10000">Loading...</td>
-              ) : (
-                <td colSpan="10000">
-                  Showing {page.length} of ~{controlledPageCount * pageSize} results
-                </td>
-              )}
-            </tr>
-          </tbody>
-        </table>
-        {/*
-          Pagination can be built however you'd like.
-          This is just a very basic UI implementation:
-        */}
-        <div className="pagination">
-          <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-            {'<<'}
-          </button>{' '}
-          <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-            {'<'}
-          </button>{' '}
-          <button onClick={() => nextPage()} disabled={!canNextPage}>
-            {'>'}
-          </button>{' '}
-          <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-            {'>>'}
-          </button>{' '}
-          <span>
-            Page{' '}
-            <strong>
-              {pageIndex + 1} of {pageOptions.length}
-            </strong>{' '}
-          </span>
-          <span>
-            | Go to page:{' '}
-            <input
-              type="number"
-              defaultValue={pageIndex + 1}
-              onChange={e => {
-                const page = e.target.value ? Number(e.target.value) - 1 : 0;
-                gotoPage(page);
-              }}
-              style={{ width: '100px' }}
-            />
-          </span>{' '}
-          <select
-            value={pageSize}
-            onChange={e => {
-              setPageSize(Number(e.target.value));
-            }}
-          >
-            {[10, 20, 30, 40, 50].map(pageSize => (
-              <option key={pageSize} value={pageSize}>
-                Show {pageSize}
-              </option>
-            ))}
-          </select>
-        </div>
-      </>
-    );
-  }
-
-  const columns = useMemo(
-    () => [
-      {
-        columns: [
-          {
-            Header: 'Title',
-            accessor: 'title',
-          },
-          {
-            Header: 'Overview',
-            accessor: 'overview',
-          },
-          {
-            Header: 'Genres',
-            accessor: 'genres',
-          },
-          {
-            Header: 'Stock Qty.',
-            accessor: 'stockQty',
-          },
-        ],
-      },
-    ],
-    []
-  );
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(1);
+  };
 
   useEffect(() => {
     async function loadMovies() {
@@ -214,56 +152,84 @@ export default function ListMoviesPage() {
           <div>Loading...</div>
         ) : (
           <>
-            {/* <Table bordered striped hover>
-              <thead>
-                <tr>
-                  <th>Title</th>
-                  <th>Overview</th>
-                  <th>Genres</th>
-                  <th>Stock Qty.</th>
-                  <th></th>
-                </tr>
-              </thead>
+            <TableContainer component={Paper}>
+              <Table className={classes.table} aria-label="custom pagination table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="center" style={{ minWidth: 170 }}>
+                      Title
+                    </TableCell>
 
-              <tbody>
-                {data.totalCount > 0 ? (
-                  data.data.map((movie, i: number) => (
-                    <tr key={i}>
-                      <td>{movie.title}</td>
-                      <td>{movie.overview}</td>
-                      <td>{movie.genres}</td>
-                      <td>{movie.availableCopies}</td>
-                      <td>
-                        <ButtonGroup>
-                          <Button color="primary">Edit</Button>
-                          <Button color="danger">Delete</Button>
-                        </ButtonGroup>
-                      </td>
-                    </tr>
-                  ))
-                ) : (
-                  <tr>
-                    <td colSpan="5">No movies found!</td>
-                  </tr>
-                )}
-              </tbody>
-            </Table>
+                    <TableCell align="center" style={{ minWidth: 170 }}>
+                      Genres
+                    </TableCell>
 
-            <div className="d-flex justify-content-end">
-              <ButtonGroup>
-                <Button>{'<'}</Button>
-                <Button>1</Button>
-                <Button>{'>'}</Button>
-              </ButtonGroup>
-            </div> */}
+                    <TableCell align="center" style={{ minWidth: 170 }}>
+                      Available copies
+                    </TableCell>
 
-            <Table
-              columns={columns}
-              data={data.data}
-              fetchData={fetchData}
-              loading={isLoading}
-              pageCount={pageCount}
-            />
+                    <TableCell align="center" style={{ minWidth: 170 }}>
+                      Actions
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+
+                <TableBody>
+                  {data.data.map(row => (
+                    <TableRow key={row.name}>
+                      <TableCell component="th" scope="row">
+                        {row.title}
+                      </TableCell>
+
+                      <TableCell style={{ width: 160 }} align="center">
+                        {row.genres.length > 0
+                          ? row.genres.map(genre => <Chip key={genre} label={genre} size="small" />)
+                          : '-'}
+                      </TableCell>
+
+                      <TableCell style={{ width: 160 }} align="center">
+                        {row.availableCopies}
+                      </TableCell>
+
+                      <TableCell style={{ width: 160 }} align="center">
+                        <IconButton aria-label="edit">
+                          <EditIcon />
+                        </IconButton>
+
+                        <IconButton aria-label="delete">
+                          <DeleteIcon />
+                        </IconButton>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+
+                  {data.totalCount === 0 && (
+                    <TableRow style={{ height: 53 * 1 }}>
+                      <TableCell colSpan={6} />
+                    </TableRow>
+                  )}
+                </TableBody>
+
+                <TableFooter>
+                  <TableRow>
+                    <TablePagination
+                      rowsPerPageOptions={[10]}
+                      colSpan={4}
+                      count={data.totalCount}
+                      rowsPerPage={rowsPerPage}
+                      page={page}
+                      SelectProps={{
+                        inputProps: { 'aria-label': 'rows per page' },
+                        native: true,
+                      }}
+                      onPageChange={handleChangePage}
+                      onRowsPerPageChange={handleChangeRowsPerPage}
+                      ActionsComponent={TablePaginationActions}
+                    />
+                  </TableRow>
+                </TableFooter>
+              </Table>
+            </TableContainer>
           </>
         )}
       </Container>
